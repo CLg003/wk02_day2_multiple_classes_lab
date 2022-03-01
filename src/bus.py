cc6a70d0440f846a5cd1d4e40f615cfe3a2f199a
CLg003 <clairemjlaing@gmail.com>
2022-03-01 14:44:56 +0000

    part b completed

--- src/bus.py
+++ src/bus.py
@@ -1,7 +1,17 @@
 class Bus:
     def __init__(self, route, destination):
-        self.route = route
+        self.route_number = route
         self.destination = destination
-    
+        self.passengers = []
     def drive(self):
         return 'Brum brum'
+    def passenger_count(self):
+        return len(self.passengers)
+    def pick_up(self, person):
+        self.passengers.append(person)
+    def drop_off(self, person):
+        self.passengers.remove(person)
+    def empty(self):
+        self.passengers = []
+    
+
